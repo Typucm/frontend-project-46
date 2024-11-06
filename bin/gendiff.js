4513223ec4c0d0d1d7930fd67eff05775531ab07
Дмитрий Горохов <zoomik05@gmail.com>
2024-11-06 11:19:32 +0300

    read

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]',  'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
   program.parse()
